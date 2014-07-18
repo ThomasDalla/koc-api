20217dbcbecf27cb9c9a0272a5f5b80f69cf1f4c
Thomas Dall'Agnese <thomas@dallagnese.fr>
2014-07-18 23:02:29 +0000

    deploy test

--- server.js
+++ server.js
@@ -20,13 +20,13 @@ var router = express.Router(); 				// get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-	res.json({ message: 'hooray! welcome to our api!' });
+	res.json({ message: 'hooray! welcome to our api! test' });
 });
 
 // more routes for our API will happen here
 
 // REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
+// all of our routes will be prefixed with /
 app.use('/', router);
 
 // START THE SERVER
