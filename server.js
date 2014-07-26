b803d82f3b498e1c2b2d8e1f27122f72b9e9f0d7
Thomas Dall'Agnese <thomas@dallagnese.fr>
2014-07-26 07:05:29 +0000

    Refactoring, base updated and implemented Login Service, fixes #3

--- server.js
+++ server.js
@@ -7,29 +7,76 @@
 var express    = require('express');        // call express
 var app        = express(); 				// define our app using express
 var bodyParser = require('body-parser');
+var KoC        = require('koc');            // the API we use to call KoC
+
+// Constants
+const KOC_SESSION_HEADER_NAME = 'x-koc-session';
 
 // configure app to use bodyParser()
-// this will let us get the data from a POST
+// this will let us easily get the data from a POST
 app.use(bodyParser());
 
-var port = process.env.PORT || 3000; 		// set our port
-
-// ROUTES FOR OUR API
+// Home
 // =============================================================================
-var router = express.Router(); 				// get an instance of the express Router
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+var router = express.Router(); // get an instance of the express Router
+router.use(express.static(__dirname + '/public'));
 router.get('/', function(req, res) {
-	res.json({ message: 'hooray! welcome to our api! test' });
+    res.sendfile(__dirname + '/public/home.html');
 });
 
-// more routes for our API will happen here
+// API
+// =============================================================================
 
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /
-app.use('/', router);
+// Get an instance of express Router
+var api = express.Router();
+
+// API Middleware to use for all requests to capture the X-KoC-Session header
+api.use(function(req, res, next) {
+    // create our KoC helper
+    res.koc = new KoC();
+    // set the session_id, if one was passed
+	if( req.headers[KOC_SESSION_HEADER_NAME] !== undefined
+	        && req.headers[KOC_SESSION_HEADER_NAME].length )
+	    res.koc.setSession(req.headers[KOC_SESSION_HEADER_NAME]);
+	next(); // make sure we go to the next routes and don't stop here
+});
+
+// API Home
+// -----------------------------------------------------------------------------
+api.get('/', function(req, res) {
+	res.json({ message: 'API Root. Read the doc to learn how to use it.' });
+});
+
+// LOGIN
+// -----------------------------------------------------------------------------
+api.route('/login').post(function(req, res) {
+	var username = req.body.username;
+	if( username === undefined || !username.length )
+	    res.json({
+	       success: false,
+	       error: "Please specify a username to login..."
+	    });
+	var password = req.body.password;
+	if( password === undefined || !password.length )
+	    res.json({
+	       success: false,
+	       error: "Please specify a password to login..."
+	    });
+    res.koc.login(username, password)
+    .then( function(result) {
+        res.json(result);
+    }).fail( function(result) {
+        res.json(result);
+    });
+});
+
+// REGISTER OUR ROUTES
+// =============================================================================
+app.use('/'   , router);
+app.use('/api', api   );
 
 // START THE SERVER
 // =============================================================================
+var port = process.env.PORT || 3000;
 app.listen(port, process.env.IP || "0.0.0.0");
 console.log('Magic happens on port ' + port);
