e8194572a037e1c5f412d508dbf7caa4e8c07575
Thomas Dall'Agnese <thomas@dallagnese.fr>
2014-08-09 08:08:06 +0000

    allow header X-Koc-Session

--- koc-api.js
+++ koc-api.js
@@ -90,7 +90,7 @@ app.all('/*', function(req, res, next){
     res.header('Access-Control-Allow-Origin', '*');
     //res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Origin');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Origin, X-KoC-Session');
     next();
 });
 
