6964d52c64360a2b6aab70acab3b6755a8239a83
Thomas Dall'Agnese <thomas@dallagnese.fr>
2015-05-03 12:31:56 +0900

    handle OpenShift PORT/IP

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = require('./koc-api');
 
 // START THE SERVER
 // =============================================================================
-var port = process.env.PORT || 3000;
-var ip   = process.env.IP   || "0.0.0.0";
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP   || "0.0.0.0";
 app.listen(port, ip);
 console.log('Magic happens on port ' + port);
